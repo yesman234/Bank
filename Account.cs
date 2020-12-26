3000aa1da1c8106a2402de3c35a7fb20eedacfb9
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2020-12-25 21:26:27 -0800

    getting cutomer class ready for deposits

--- Account.cs
+++ Account.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace BankClassLibrary
 {
 
-    public partial class Account
+    public class Account
     {
         Customer AccountCustomer; // The Account Holder
 
@@ -22,16 +22,15 @@ namespace BankClassLibrary
 
         }
 
-    }
-    public partial class Account
-    {
+    
+    
         int AccountNumber;
-       
+
         public double CurrentBalance;
-        List<double> ListOfTransactions;
+        List<Transaction> ListOfTransactions;
 
         // Initialization
-        public Account(string aCustomerName, DateTime aDateOfBirth, string aPhone = null, string aAddress = null )
+        public Account(string aCustomerName, DateTime aDateOfBirth, string aPhone = null, string aAddress = null)
         {
             CustomerName = aCustomerName;
             DateofBirth = aDateOfBirth;
@@ -41,36 +40,47 @@ namespace BankClassLibrary
             AccountNumber = Guid.NewGuid().GetHashCode();
 
             CurrentBalance = 0;
-            ListOfTransactions = new List<double>();
 
+            ListOfTransactions = new List<Transaction>();
+
+
+            // A method to display balance
+            void DisplayBalance(double aTest)
+            {
+
+            }
+
+            //Deposit money method
+            bool DepositMoneyType(double aAmount)
+            {
+                bool isSuccess = false;
+
+                return isSuccess;
+            }
+
+            //Withdraw money method
+            bool WithdrawMoneyType(double aAmount)
+            {
+                bool isSuccess = false;
+
+                return isSuccess;
+            }
+            
+           
+           
         }
-
-        // A method to display balance
-        void DisplayBalance(double aTest)
-        {
-
-        }
-
-        //Deposit money method
-        bool DepositMoneyType(double aAmount)
-        {
-            bool isSuccess = false;
-
-            return isSuccess;
-        }
-
-        //Withdraw money method
-        bool WithdrawMoneyType(double aAmount)
-        {
-            bool isSuccess = false;
-
-            return isSuccess;
-        }
-
         public class Transaction
         {
-
+            public double AmountOfTransaction;
+            public DateTime TransactionDate;
+            public string Location;
+            public TransactionType TypeOfTranactions;
         }
-       
+        
+        public enum TransactionType
+        {
+            DEPOSIT,
+            WITHDRAWL
+        }
     }
 }
